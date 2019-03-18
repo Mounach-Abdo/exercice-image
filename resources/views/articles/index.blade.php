10366ac7c58cc8e370e2fa9763c5d32dceb646da
Mounach-Abdo <mounach-hotmail.com>
2019-03-18 10:31:43 +0100

    changes

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+@section('content')
+<div class="container">
+    <div class="card">
+        <div class="card-title">
+            
+        </div>
+    </div>
+</div>
+@endsection
